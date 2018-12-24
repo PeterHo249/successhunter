f6a161562d131bf6495a0a609efb2f264ab4ae15
PETERHO\PeterHo <peter_ho_249@outlook.com>
2018-12-24 15:04:05 +0700

    add shared pref and init intro

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intro_views_flutter/Models/page_view_model.dart';
+import 'package:intro_views_flutter/intro_views_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'ui/login_page.dart';
 import 'package:successhunter/ui/splash_page.dart';
@@ -18,7 +21,50 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'Roboto',
       ),
-      home: _handleCurrentScreen(),
+      home: HomeWidget(),
+    );
+  }
+}
+
+class HomeWidget extends StatefulWidget {
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  Future<bool> _isAlreadyIntro;
+
+  @override
+  void initState() {
+    _isAlreadyIntro = _prefs.then((SharedPreferences prefs) {
+      print('in get init state');
+      return prefs.getBool('isAlreadyIntro') ?? false;
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('in build');
+    return _buildHome(context);
+  }
+
+  Widget _buildHome(BuildContext context) {
+    return FutureBuilder(
+      future: _isAlreadyIntro,
+      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+        print('in future builder');
+        print(snapshot.data);
+        if (ConnectionState.waiting == snapshot.connectionState) {
+          return SplashPage();
+        } else {
+          if (snapshot.data) {
+            return _handleCurrentScreen();
+          } else {
+            return _buildIntro(context);
+          }
+        }
+      },
     );
   }
 
@@ -40,5 +86,37 @@ class MyApp extends StatelessWidget {
         }
       },
     );
+  }
+
+  Widget _buildIntro(BuildContext context) {
+    print('in build intro');
+    return IntroViewsFlutter(
+      _buildPageViewModel(context),
+      onTapDoneButton: () {
+        _prefs.then((SharedPreferences prefs) {
+          prefs.setBool('isAlreadyIntro', true);
+        });
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => _handleCurrentScreen()));
+      },
+      showSkipButton: false,
+    );
+  }
+
+  List<PageViewModel> _buildPageViewModel(BuildContext context) {
+    return <PageViewModel>[
+      PageViewModel(
+        pageColor: Colors.amber,
+        title: Text('Test Intro'),
+        mainImage: Image.asset(
+          'assets/avatar/boss_1.png',
+          height: 250.0,
+          width: 250.0,
+          alignment: Alignment.center,
+          fit: BoxFit.contain,
+        ),
+        body: Text('Something to intro my app'),
+      ),
+    ];
   }
 }
